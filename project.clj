3520270a899809a5a2b9b108779a6822c635c48e
Aaron Steele <eightysteele@gmail.com>
2013-02-27 13:30:53 -0800

    fix project.clj :excludes on org.slf4j

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
   :jvm-opts ["-XX:MaxPermSize=256M"
              "-XX:+UseConcMarkSweepGC"
              "-Xms1024M" "-Xmx1048M" "-server"]
-  :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev" :exclusions [org.slf4j/slf4j-log4j12]]
+  :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                                   [eightysteele/midje-cascalog "0.5.0"]]
                    :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}
              :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}
@@ -23,6 +23,7 @@
                  [cascading/cascading-hadoop "2.2.0-wip-19" :exclusions [org.slf4j/slf4j-log4j12]]
                  [org.pingles/cascading.protobuf "0.0.1"]
                  [dwca-reader-clj "0.7.0-SNAPSHOT"]
+                 [org.clojure/data.json "0.2.1"]
                  ;;[cartodb-clj "1.5.2"]
                  [org.clojure/data.csv "0.1.2"]
                  [clj-http "0.4.3"]
