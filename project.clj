44115fed4b44d6f38665d25c28f54c130d57b81f
Robin Kraft <rkraft4@gmail.com>
2013-03-01 19:36:51 -0800

    Updated deps

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
   :jvm-opts ["-XX:MaxPermSize=256M"
              "-XX:+UseConcMarkSweepGC"
              "-Xms1024M" "-Xmx1048M" "-server"]
-  :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev" :exclusions [org.slf4j/slf4j-log4j12]]
+  :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                                   [eightysteele/midje-cascalog "0.5.0"]]
                    :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}
              :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}
@@ -20,11 +20,11 @@
                  [backtype/dfs-datastores "1.2.0"]
                  [cascalog-more-taps "0.3.1-SNAPSHOT"]
                  [backtype/dfs-datastores-cascading "1.3.0"]                 
-                 [cascading/cascading-hadoop "2.2.0-wip-19"]
+                 [cascading/cascading-hadoop "2.2.0-wip-19" :exclusions [org.slf4j/slf4j-api org.slf4j/slf4j-log4j12]]
                  [org.pingles/cascading.protobuf "0.0.1"]
                  [dwca-reader-clj "0.7.0-SNAPSHOT"]
                  [org.clojure/data.json "0.2.1"]
-                 [cartodb-clj "1.5.2"]
+                 [cartodb-clj "1.5.3"]
                  [org.clojure/data.csv "0.1.2"]
                  [clj-http "0.4.3"]
                  [net.lingala.zip4j/zip4j "1.3.1"]
@@ -33,7 +33,6 @@
                  ;;[clj-aws-s3 "0.3.2"]
                  [org.gbif/gbif-metadata-profile "1.1-SNAPSHOT"]
                  [enlive "1.0.1"]
-                 [org.apache.thrift/libthrift "0.8.0"
-                  :exclusions [org.slf4j/slf4j-api]]]
+                 [org.apache.thrift/libthrift "0.8.0"]]
   :min-lein-version "2.0.0"
   :aot [vn.schema gulo.hadoop.pail org.json])
