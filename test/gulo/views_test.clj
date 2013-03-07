5cb0de206fb7798d1d494ae7c7b864a6851eed04
Robin Kraft <rkraft4@gmail.com>
2013-03-06 19:52:50 -0800

    Added tests and future facts for views.clj

--- test/gulo/views_test.clj
+++ test/gulo/views_test.clj
@@ -2,64 +2,76 @@
   (:use gulo.views :reload)
   (:use [cascalog.api]
         [gulo.thrift :as t]
-        [gulo.hadoop.pail :as p])
-  (:require [cascalog.ops :as c]
-            [midje sweet cascalog])
+        [gulo.hadoop.pail :as p]
+        [midje sweet cascalog])
+  (:require [cascalog.ops :as c])
   (:import [backtype.hadoop.pail Pail]))
 
-;; needed fields
+(def PAIL-PATH "/tmp/vn-test-tmp")
 
-;; RecordProperty
-;;;;;;;;;;;;;;;;;;;
+(def TEST-RECORDS
+  [{:id "myid" :scientificname "Puma Concolor" :country "United States" :collectioncode "MVZ" :clazz "Mammalia"}
+   {:id "myid" :scientificname "Puma Concolor" :country "United States" :collectioncode "MVZ" :clazz "Mammalia"}
+   {:id "yourid" :scientificname "Mustache Mustachus" :country "Mustandia" :collectioncode "VIZZ" :clazz "Mustachia"}])
 
-;; ScientificName
-;; SourceID
-;; Country
-;; CollectionCode
-;; Clazz
+(defn sink-test-records [pail-path records]
+  (let [dataset-guid "myguid"
+        record-data (map (partial t/record-data dataset-guid) records)]
+    (for [d record-data]
+      (p/to-pail pail-path (<- [?d] (d ?d))))))
 
-;; OrganizationProperty
-;;;;;;;;;;;;;;;;;;;;;;;;
-;; Uuid
+(defn delete-directory
+  [path]
+  (let [fs (file-seq (clojure.java.io/file path))]
+    (for [f (reverse fs)]
+      (clojure.java.io/delete-file f))))
 
+(defn query-runner
+  [fun k pail-path]
+  (let [tap (p/split-chunk-tap pail-path (k stats-paths))]
+    (fun tap)))
 
-(defn get-n-sample-records
-  "Grab n records from the sample pail."
-  [n & [p]]
-  (let [path (str "/tmp/vn/" p)]
-    (take n (Pail. path))))
+;; TODO: ensure sink-test-records runs before tests
+(sink-test-records PAIL-PATH TEST-RECORDS)
 
-(comment
-  ;; records by country
-  (let [tap (p/split-chunk-tap "/tmp/vn/" (:records-by-country stats-paths))]
-    (??- (total-occ-by-country-query tap)))
+(future-fact "Checks unpack-RecordProperty")
+(future-fact "Checks get-RecordProperty-id")
+(future-fact "Checks unpack-OrganizationProperty")
+(future-fact "Checks get-OrganizationProperty-id")
+(future-fact "Checks get-org-id")
+(future-fact "Checks get-country")
+(future-fact "Checks get-scientific-name")
+(future-fact "Checks get-collection-code")
+(future-fact "Checks get-class")
+(future-fact "Checks get-unique-sci-names")
+(future-fact "Checks get-unique-occurrences")
+(future-fact "Checks get-unique-occ-by-country")
+(future-fact "Checks get-unique-publishers")
+(future-fact "Checks get-unique-by-coll-code")
+(future-fact "Checks get-unique-by-occ-class")
 
-  ;; count records
-  (let [tap (p/split-chunk-tap "/tmp/vn/" (:total-records stats-paths))]
-    (??- (total-occurrences-query tap)))
+(fact "Checks total-occ-by-country-query."
+  (query-runner total-occ-by-country-query :records-by-country PAIL-PATH)
+  => (produces [["Mustandia" 1] ["United States" 1]]))
 
-  ;; total publishers
-  (let [tap (p/split-chunk-tap "/tmp/vn/" (:total-publishers stats-paths))]
-    (??- (total-publishers-query tap)))
+(fact "Checks total-occurrences-query."
+  (query-runner total-occurrences-query :total-records PAIL-PATH)
+  => (produces [[2]]))
 
-  ;; total taxa
-  (let [tap (p/split-chunk-tap "/tmp/vn/" (:total-taxa stats-paths))]
-    (??- (taxa-count-query tap)))
+(future-fact "Checks total-pubishers-query (requires creating organization dataset)"
+             (query-runner total-publishers-query :total-publishers PAIL-PATH))
 
-  ;; records by collection
-  (let [tap (p/split-chunk-tap "/tmp/vn/" (:records-by-collection stats-paths))]
-    (??- (total-by-collection-query tap)))
+(fact "Checks taxa-count-query."
+  (query-runner taxa-count-query :total-taxa PAIL-PATH)
+  => (produces [[2]]))
 
-  ;; records by class
-  (let [tap (p/split-chunk-tap "/tmp/vn/" (:records-by-class stats-paths))]
-    (??- (total-by-class-query tap))))
+(fact "Checks total-by-collection-query."
+  (query-runner total-by-collection-query :records-by-collection PAIL-PATH)
+  => (produces [["MVZ" 1] ["VIZZ" 1]]))
 
-(comment
-  (defn test-data
-    []
-    (let [resource-url "http://ipt.vertnet.org:8080/ipt/resource.do?r=ttrs_mammals"
-          sql (format "SELECT author,link,eml,dwca,orgcontact,orgname,orgurl,guid,description,pubdate,publisher,name FROM resources WHERE link = '%s'" resource-url)
-          resource (first (:rows (cartodb/query sql "vertnet" :api-key api-key)))
-          {:keys [resource dataset organization]} (resource->metadata resource)]
-      (sink-metadata "/tmp/vn" resource dataset organization)
-      (sink-data "/tmp/vn" resource))))
+(fact "Checks total-by-class-query."
+  (query-runner total-by-class-query :records-by-class PAIL-PATH)
+  => (produces [["Mammalia" 1] ["Mustachia" 1]]))
+
+;; TODO: ensure delete-directory runs after tests
+;; (delete-directory PAIL-PATH)
