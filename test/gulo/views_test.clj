8da779aaac84fea125dc7f4f955d1dd428e2d4e5
Robin Kraft <rkraft4@gmail.com>
2013-04-10 11:16:34 -0700

    Pulling in changes lost in pull req from feature/new-stats-queries

--- test/gulo/views_test.clj
+++ test/gulo/views_test.clj
@@ -4,51 +4,43 @@
         [gulo.thrift :as t]
         [gulo.hadoop.pail :as p]
         [midje sweet cascalog])
-  (:require [cascalog.ops :as c])
-  (:import [backtype.hadoop.pail Pail]
-           [gulo.schema Pedigree]))
+  (:require [cascalog.ops :as c]
+            [clojure.java.io :as io]))
 
-(def PAIL-PATH "/tmp/vn-test-tmp")
-
-(def TEST-RECORDS
-  [{:id "myid" :scientificname "Puma Concolor" :country "United States" :collectioncode "MVZ" :clazz "Mammalia"}
-   {:id "myid2" :scientificname "Puma Concolor" :country "United States" :collectioncode "MVZ" :clazz "Mammalia"}
-   {:id "yourid" :scientificname "Mustache Mustachus" :country "Mustandia" :collectioncode "VIZZ" :clazz "Mustachia"}])
-
-(defn sink-test-records [pail-path records]
-  (let [dataset-guid "myguid"
-        record-data (map (partial t/record-data dataset-guid) records)]
-    (p/to-pail pail-path (vec (map vector (flatten record-data))))))
-
-(defn delete-directory
-  [path]
-  (let [fs (file-seq (clojure.java.io/file path))]
-    (for [f (reverse fs)]
-      (clojure.java.io/delete-file f))))
+(def src (hfs-textline (.getPath (io/resource "test-stats"))))
 
 (defn query-runner
   [fun k pail-path]
   (let [tap (p/split-chunk-tap pail-path (k stats-paths))]
     (fun tap)))
 
-(def test-data
-  (sink-test-records PAIL-PATH TEST-RECORDS))
+(fact "Checks get-unique-sci-names"
+  (let []
+    (get-unique-sci-names src))
+  => (produces [["Mustache Mustachus"] ["Puma Concolor"]]))
 
-(future-fact "Checks unpack-RecordProperty")
-(future-fact "Checks get-RecordProperty-id")
-(future-fact "Checks unpack-OrganizationProperty")
-(future-fact "Checks get-OrganizationProperty-id")
-(future-fact "Checks get-org-id")
-(future-fact "Checks get-country")
-(future-fact "Checks get-scientific-name")
-(future-fact "Checks get-collection-code")
-(future-fact "Checks get-class")
-(future-fact "Checks get-unique-sci-names")
-(future-fact "Checks get-unique-occurrences")
-(future-fact "Checks get-unique-occ-by-country")
-(future-fact "Checks get-unique-publishers")
-(future-fact "Checks get-unique-by-coll-code")
-(future-fact "Checks get-unique-by-occ-class")
+(fact "Checks get-unique-occurrences"
+  (let [src (p/split-chunk-tap PAIL-PATH ["prop" "RecordProperty" "Occurrence"])]
+    (get-unique-occurrences src)) => (produces [["myid"] ["yourid"]]))
+
+(fact "Checks get-unique-occ-by-country"
+  (let [src (p/split-chunk-tap PAIL-PATH ["prop" "RecordProperty" "Location"])]
+    (get-unique-occ-by-country src))
+  => (produces [["yourid" "Mustandia"] ["myid" "United States"]]))
+
+(fact "Checks get-unique-by-coll-code"
+  (let [src (p/split-chunk-tap PAIL-PATH ["prop" "RecordProperty" "RecordLevel"])]
+    (get-unique-by-coll-code src))
+  => (produces [["MVZ" "myid"] ["VIZZ" "yourid"]]))
+
+(fact "Checks get-unique-by-occ-class"
+  (let [src (p/split-chunk-tap PAIL-PATH ["prop" "RecordProperty" "Taxon"])]
+    (get-unique-by-occ-class src))
+    => (produces [["myid" "Mammalia"] ["yourid" "Mustachia"]]))
+
+(fact "Checks get-unique-publishers"
+  (let [src (p/split-chunk-tap PAIL-PATH ["prop" "OrganizationProperty"])]
+    (get-unique-publishers src)) => (produces [["AMNHguid"] ["MVZguid"]]))
 
 (fact "Checks total-occ-by-country-query."
   (query-runner total-occ-by-country-query :records-by-country PAIL-PATH)
@@ -58,8 +50,10 @@
   (query-runner total-occurrences-query :total-records PAIL-PATH)
   => (produces [[2]]))
 
-(future-fact "Checks total-pubishers-query (requires creating organization dataset)"
-             (query-runner total-publishers-query :total-publishers PAIL-PATH))
+(fact
+ "Checks total-pubishers-query"
+ (query-runner total-publishers-query :total-publishers PAIL-PATH)
+ => (produces [[2]]))
 
 (fact "Checks taxa-count-query."
   (query-runner taxa-count-query :total-taxa PAIL-PATH)
@@ -72,43 +66,3 @@
 (fact "Checks total-by-class-query."
   (query-runner total-by-class-query :records-by-class PAIL-PATH)
   => (produces [["Mammalia" 1] ["Mustachia" 1]]))
-
-;; TODO: ensure delete-directory runs after tests
-;; (delete-directory PAIL-PATH)
-
-(fact "Checks `get-most-recent-fields`."
-  (let [dataset-guid "myguid"
-        data (first (nth (first (map (partial t/record-data dataset-guid) TEST-RECORDS)) 2))
-        time (first (t/unpack (first (t/unpack data))))
-        data1 (-> data (.setPedigree (Pedigree. 1)))
-        time1 (first (t/unpack (first (t/unpack data))))]
-    (get-most-recent-fields [[time data] [time1 data1]]))
-  => [[[nil nil nil "United States" nil nil nil nil nil nil nil nil nil nil nil
-        nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil
-        nil nil nil nil nil nil nil nil nil nil nil]]])
-
-(fact "Checks `keep-most-recent` query."
-  (let [dataset-guid "myguid"
-        src (first (map (partial t/record-data dataset-guid) TEST-RECORDS))
-        src (flatten (concat src src))
-        src (map vector (repeat (count src) "") src)
-        output (keep-most-recent src)]
-    ;; check an actual output tuple
-    output
-    => (produces-some [["myid" "class gulo.schema.Event"
-                        [nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil]]])
-    ;; check number of output records - should be 9, corresponding to
-    ;; different RecordProperty types (e.g. Taxon, Event, etc.)
-    (count (first (??- output))) => 9))
-
-(fact "Check `concat-fields`"
-  (concat-fields [[1 2 3] [4 5 6]]) => [[[1 2 3 4 5 6]]])
-
-(fact "Checks `concat-fields-query`.
-
-       Note that this test depends on the records in the pail, which
-       in this simple case all have the same Pedigree. The test for
-       `keep-most-recent` checks that only the most recent records are
-       retained."
-  (let [output (??- (concat-fields-query PAIL-PATH ["prop" "RecordProperty"]))]
-    (count (last (ffirst output)))) => 156)
